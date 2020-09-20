6d067646908d85650379102a0dad52b15fc43f71
tokano864 <55218941+tokano864@users.noreply.github.com>
2020-09-20 23:24:53 +0900

    Update mission_4-1.php

--- mission4/mission_4-1.php
+++ mission4/mission_4-1.php
@@ -1,7 +1,7 @@
 <?php 
     // DB接続設定
-	$dsn = 'mysql:dbname=tb220531db;host=localhost';
-	$user = 'tb-220531';
-	$password = 'mTRmcFfg8Y';
+	$dsn = 'mysql:dbname=******;host=*******';
+	$user = '******';
+	$password = '******';
 	$pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 ?>
