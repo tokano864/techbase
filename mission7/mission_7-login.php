4fed27de587f0fc3563632f2c155708547517c3b
tokano864 <55218941+tokano864@users.noreply.github.com>
2020-10-04 17:14:40 +0900

    Add files via upload

---
+++ mission7/mission_7-login.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>タイトル</title>
+  </head>
+  <body>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+ 
+
+    <!--タイトル-->
+    <header class="top">
+        <div class="container">
+            <h1 class="header-title">
+                <a href="">タイトル</a>
+            </h1>
+            <p class="regist-title">ログイン画面</p>
+        </div>
+    </header>
+    <!--ログインフォーム-->
+    <div class="container">
+        <form action="" class="login">
+            <p class="name">名前</p>
+            <input type="text" name="name" class="name-text">
+            <p class="pass">パスワード</p>
+            <input type="text" name="pass" class="pass-text">
+            <input type="submit" class="login-btn">
+        </form>
+    </div>
+    
+    <footer>
+        <div class="container">
+            <small>&copy; ○○○</small>
+        </div>
+    </footer>
+
+  </body>
+</html>
