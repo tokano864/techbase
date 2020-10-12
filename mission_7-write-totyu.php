147aa1729612ea7603b08fdf9e9bb8b548930945
tokano864 <55218941+tokano864@users.noreply.github.com>
2020-10-12 23:46:30 +0900

    Update mission_7-write-totyu.php

--- mission_7-write-totyu.php
+++ mission_7-write-totyu.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/reset.css">
     <link rel="stylesheet" href="./css/style.css">
-    <title>タイトル</title>
+    <title>1016's kitchen</title>
   </head>
   <body>
 
@@ -19,21 +19,31 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
  
-    <!--ログインしているかどうか判定、-->
- 
     <!--データベース接続およびお試しのための名前の宣言-->
         <?php
-            $dsn = 'mysql:dbname=*****;host=*******';
-	        $user = '*****';
-	        $password = '*********';
+            $dsn = 'mysql:dbname=*******;host=*******';
+	        $user = '******';
+	        $password = '*******';
 	        $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 
-            //値の受け渡しがないのでとりあえず名無し太郎という投稿者名にする
+            //値の受け渡しがないのでとりあえずuseridを1、名無し太郎という投稿者名にする
                 $name="名無し太郎";
                 $userid=1;
         ?>
         
     <!--検索機能-->
+    <?php
+        //検索するワードを取得
+        $serch =$_POST["serch"];
+        //テキストと一部一緒の部分のデータすべてを取得
+        $sql='SELECT*FROM recipe WHERE food LIKE :food OR recipe LIKE :recipe';
+        $stmt=$pdo->prepare($sql);
+        $stmt->bindParam(':food',$serch,PDO::PARAM_STR);
+        $stmt->bindParam(':recipe',$serch,PDO::PARAM_STR);
+        $stmt->execute();
+        $serchresults=$stmt->fetchAll();
+
+    ?>
 
     <!--カテゴリー一覧表示機能-->
 
@@ -70,10 +80,51 @@
             $sql->bindParam(':recipe',$recipe,PDO::PARAM_STR);
             $sql->bindParam(':category',$foodcategory,PDO::PARAM_STR);
             $sql->execute();
-        
-        
+            
+            //投稿番号を取得
+            $sql = 'SELECT * FROM recipe';
+	            $stmt = $pdo->query($sql);
+	            $results = $stmt->fetchAll();
+	            foreach ($results as $row){
+		            $recipeid= $row['id'];
+	                }	
+            //テーブル(recipe_img)を作成
+            $sql="CREATE TABLE IF NOT EXISTS recipe_img"
+                    ."("
+                    ."id INT,"
+                    ."img BLOB,"
+                    ."ext VARCHAR(5)"
+                    .")";
+            $stmt=$pdo->query($sql);
+           //画像取得
+           $upfile=$_FILES["upfile"]["tmp_name"];
+           $img=file_get_contents($upfile);
+           $ext=pathinfo($upfaile,PATHINFO_EXTENSION);
+            
+            //画像を保存
+            $sql=$pdo->prepare("INSERT INTO recipe_img
+                    (id,img,ext) VALUES (:id,:img,:ext)");
+            $sql->bindParam(':id',$recipeid,PDO::PARAM_INT);
+            $sql->bindParam(':img',$img);
+            $sql->bindParam(':ext',$ext);
+            $stmt->execute();
+            
+            //テスト用(recipe_img)
+            $sql='SELECT * FROM recipe_img WHERE id=:id';
+            $stmt=$pdo->prepare($sql);
+            $stmt->bindParam('id',$recipeid,PDO::PARAM_INT);
+            $stmt->execute();
+            $contents_type=array(
+                'jpg'=>'image/jpeg',
+                'jpeg'=>'image/jpeg',
+                'png'=>'image/jpng',
+                'gif'=>'image/gif',
+                'bmp'=>'image/bmp');
+            $img=$stmt->fetchObject();
+            header('Content-type:'.$contents_type[$img->ext]);
+            echo $img->contents;
         }
-	       
+	       //テスト用(recipe)
          $sql = 'SELECT * FROM recipe';
 	            $stmt = $pdo->query($sql);
 	            $results = $stmt->fetchAll();
@@ -82,6 +133,7 @@
 		            echo $row['id'].',';
 		            echo $row['user_id'].',';
 		            echo $row['user_name'].',';
+		            echo $row['food'].',';
 		            echo $row['category'];
 		            echo $row['recipe'];
 		            echo $row['time'];
@@ -117,18 +169,24 @@
         <i class="fas fa-times-circle"></i>閉じる
       </label>
       <ul class="login">
+          <!--ログインされていないとき-->
+        <?php if(!isset($_SESSION['name'])):?>
         <li>
           <a href="#">ユーザー登録</a>
         </li>
         <li>
           <a href="#">ログイン</a>
-        </li>
+        </li><!--トップページに戻る-->
+        <?php //header("Location:mission_7-top.php");?>
+        <!--ログインされていないとき-->
+        <?php else:?>
         <li>
           <a href="#">レシピ投稿</a>
         </li>
         <li>
           <a href="#">管理者専用</a>
         </li>
+        <?php endif;?>
       </ul>
     </nav>
 
@@ -146,20 +204,21 @@
     <div class="main">
         <section class="serch">
             <!--検索フォーム-->
-            <form action="" class="serch">
+            <form action="" class="serch" method="post">
                 <input type="text" name="serch" class="serch-text">
-                <input type="submit" class="serch-btn">
+                <input type="submit" class="serch-btn" name="serch-btn">
             </form>
+            <?php if($_POST["serch-btn"]): //検索ボタンが押された時?>
             <!--検索結果表示-->
                 <div class="serch-result">
-                    <p class="serch-title">検索結果</p>
-                    <a href="#" class="serch-food">料理名</a>
-                    <p class="serch-writer">投稿者</p>
-                    <img class="serch-img" src="#" alt="#" title="#"/>
+                        <p class="serch-title">検索結果</p>
+                    <?php foreach($serchresults as $row):?>
+                        <a href="#" class="serch-food"><?php echo $row['food'];?></a>
+                        <p class="serch-writer"><?php echo $row['user_name'];?></p>
+                        <img class="serch-img" src="#" alt="#" title="#"/>
+                    <?php endforeach;?>
                 </div>
-            <form>
-                <!--PHPにて表示-->
-            </form>
+            <?php endif;?>
         </section>
         <section class="write">
             <div class="container">
